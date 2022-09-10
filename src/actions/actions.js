0fec2d9c6f6f882588066ec3442dfae8a2e62389
anwarderbali <anwarderbali58@gmail.com>
2022-09-10 11:55:45 +0200

    cp-reduxr

---
+++ src/actions/actions.js
@@ -0,0 +1,15 @@
+export const addtodo =()=>{
+    return{
+        type:"ADD-TODO"
+    }
+}
+export const delatetodo =()=>{
+    return{
+        type:"DELATE-TODO"
+    }
+}
+export const removetodo =()=>{
+    return{
+        type:"REMOVE-TODO"
+    }
+}
---
+++ src/store/store.js
@@ -0,0 +1,6 @@
+
+import { combineReducers, createStore } from "redux";
+import reducer from '../reducer/reducer'
+const store = createStore(combineReducers({reducer}), +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+export default store;
